8e2eab5885b5f263f20f3a0ee883d6a7325311fb
aligomaa56 <3looosh56565656@gmail.com>
2024-02-10 00:43:21 +0200

    done

---
+++ models/review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a BaseModel class"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """The Review class"""
+    place_id = ''
+    user_id = ''
+    text = ''
---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a BaseModel class"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """The State class"""
+    name = ''
